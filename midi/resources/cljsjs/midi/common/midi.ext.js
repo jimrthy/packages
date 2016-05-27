c64b5e8ee1844ee008d7ffcb6444c20931521881
James Gatannah <james.gatannah@gmail.com>
2016-05-26 22:59:28 -0500

    Generate externs for midi.js
    Probably not right, but it's a first step

---
+++ midi/resources/cljsjs/midi/common/midi.ext.js
@@ -0,0 +1,15 @@
+var MIDI = {
+    "audioDetect": function(){},
+    "Soundfont": {},
+    "Player": {},
+    "DEBUG": {},
+    "USE_XHR": {},
+    "soundfontUrl": {},
+    "loadPlugin": {},
+    "loadResource": {},
+    "setDefaultPlugin": {},
+    "AudioTag": {},
+    "WebAudio": {},
+    "WebMIDI": {},
+    "Synesthesia": {}
+};
